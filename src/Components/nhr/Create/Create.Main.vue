dfe33af4b8e8cb4f8f5cdf20932774c2e3b8cc2b
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-06-12 13:05:25 +0800

    added the component

--- src/Components/nhr/Create/Create.Main.vue
+++ src/Components/nhr/Create/Create.Main.vue
@@ -1,14 +1,23 @@
 <template>
   <section>
-    {{ this.userName }}
+    {{ this.inputValue }}
+
+    <ReusablesCustomInput v-model="inputValue" />
   </section>
 </template>
 
 <script>
+import ReusablesCustomInput from "./Components/Reusables.CustomInput.vue";
+
 export default {
+  components: {
+    ReusablesCustomInput,
+  },
+
   data() {
     return {
       userName: "adadsfsdfsf",
+      inputValue: "",
     };
   },
 };
