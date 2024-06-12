59799e0ee984302de4e43be836752cde9c722b00
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-06-12 12:16:31 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <NhrCreateMain />
+  </div>
+</template>
+
+<script>
+import NhrCreateMain from "./Components/nhr/Create/Create.Main.vue";
+
+export default {
+  components: {
+    NhrCreateMain,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/Components/nhr/Create/Create.Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    {{ this.userName }}
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    userName: "adsf";
+  },
+};
+</script>
