badc81bddee6bf87f36271c7927cda26f3917187
Jacob Saucelo <jacobsaucelo32@gmail.com>
2024-06-12 13:46:58 +0800

    added main for the tester

---
+++ src/Components/tester/main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ChildComponent v-model:name="name" v-model:age="age" v-model:sex="sex" />
+  </div>
+</template>
+
+<script>
+import ChildComponent from "./ChildComponent.vue";
+
+export default {
+  components: {
+    ChildComponent,
+  },
+  data() {
+    return {
+      name: "",
+      age: "",
+      sex: "",
+    };
+  },
+};
+</script>
